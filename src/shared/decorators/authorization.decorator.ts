0348b1b6e211bc81893ce4c7414e779c989c7dca
AdaptingAFM <ca.lesde@gmail.com>
2022-03-11 05:25:51 -0300

    :building_construction: auth decorator: interfaces in situ

--- src/shared/decorators/authorization.decorator.ts
+++ src/shared/decorators/authorization.decorator.ts
@@ -1,5 +1,15 @@
-import { AuthorizationHandler } from 'src/shared';
+import { AppAbility } from 'src/shared';
 import { SetMetadata } from '@nestjs/common';
+
+export interface IAuthorizationHandler {
+  handle(ability: AppAbility): boolean;
+}
+
+type AuthorizationHandlerCallback = (ability: AppAbility) => boolean;
+
+export type AuthorizationHandler =
+  | IAuthorizationHandler
+  | AuthorizationHandlerCallback;
 
 export const AUTHORIZATION_METADATA_KEY = 'authorization_checks';
 export const AuthorizationRequire = (...handlers: AuthorizationHandler[]) =>
