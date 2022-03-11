5cac7867b443e0232e8f468a333288c6dc5d90b5
AdaptingAFM <ca.lesde@gmail.com>
2022-03-11 04:49:37 -0300

    :sparkles: src/database: export services

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,18 @@
+import { MeetingsService } from './meetings';
 import { Module } from '@nestjs/common';
+import { UsersService } from './users';
 
-@Module({})
+@Module({
+  providers: [UsersService, MeetingsService],
+  exports: [
+    {
+      provide: 'SRV:USERS',
+      useClass: UsersService,
+    },
+    {
+      provide: 'SRV:MEETINGS',
+      useClass: MeetingsService,
+    },
+  ],
+})
 export class DatabaseModule {}
